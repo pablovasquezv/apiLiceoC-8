34512a9ebf48dc2c0a7074cabd39c5bd180a1c08
Pablov <juan.vasquez@valposystem.com>
2023-08-07 20:45:45 -0400

    Creación de nuevos comentarios para detallar las consultas JPQL

--- src/main/java/com/complejoeducaciona/repository/IAlumnoRepository.java
+++ src/main/java/com/complejoeducaciona/repository/IAlumnoRepository.java
@@ -18,15 +18,67 @@ import java.util.List;
  */
 @Repository
 public interface IAlumnoRepository extends JpaRepository<Alumno, Long> {
-    // Seleccionar todas la instancias desde la BD y haciendo un Join con la tabla Apoderado
+    /**
+     * En este ejemplo, estamos utilizando Spring Data JPA para definir un repositorio de libros (LibroRepository).
+     * La interfaz extiende JpaRepository y proporciona métodos predefinidos para realizar operaciones CRUD en la
+     * entidad Libro.
+     *
+     * Luego, utilizamos la anotación @Query para especificar una consulta JPQL personalizada. La consulta selecciona
+     * todos los libros (select a from Alumno a ), y utiliza la cláusula left join fetch para realizar una carga eager
+     * de las entidades relacionadas apoderado y domicilio.
+     *
+     * La consulta toma un argumento adicional Sort sort, que puedes usar para ordenar los resultados.
+     * @param sort
+     * @return
+     */
     @Query(value = "select a from Alumno a left join fetch a.apoderado e left join a.domicilio")
-    public List<Alumno> findAll(Sort sort);
+    public List<Alumno> findAllSortAlumno(Sort sort);
 
-    // Contar la cantidad de Alumnos que vienen en la búsqueda y páginarlos
+
+    /***
+     * En esta consulta, hemos utilizado Page en lugar de List para obtener resultados paginados.
+     * La anotación @Query tiene dos parámetros: value para la consulta principal y countQuery para la consulta de
+     * conteo de registros.
+     *
+     * En la consulta principal, estamos seleccionando los Alumnos y utilizando las cláusulas LEFT JOIN FETCH para
+     * realizar una carga eager de las entidades relacionadas apoderado y domicilio.
+     *
+     * En la consulta de conteo de registros, estamos utilizando la función COUNT() para contar el número total de
+     * registros en la tabla Alumno.
+     *
+     * La interfaz LibroRepository extiende JpaRepository e incluye el método findAll() que toma un
+     * objeto Pageable como parámetro y devuelve una instancia de Page<Alumno>. Esto te permitirá obtener resultados
+     * paginados y acceder a información adicional sobre la paginación, como el número total de páginas, el número t
+     * otal de elementos, etc.
+     * @param pageable
+     * @return
+     */
     @Query(value = "select a from Alumno a left join fetch a.apoderado e left join a.domicilio",
             countQuery = "select (a) from Alumno a left join a.apoderado e left join a.domicilio")
-    public Page<Alumno> findAll(Pageable pageable);
-
+    public Page<Alumno> findAllPageAlumno(Pageable pageable);
+    /**
+     * En esta consulta, la anotación @Query para especificar una consulta personalizada en JPQL para buscar un Alumno por
+     * su ID. Aquí está la explicación de cada componente de la consulta:
+     *
+     * select a from Alumno a: esta parte de la consulta especifica que queremos seleccionar la entidad Alumno y asignarle
+     * el alias a para su uso en la consulta.
+     *
+     * left join fetch a.apoderardo e: esta cláusula left join fetch se utiliza para realizar un join con la entidad
+     * Apoderado relacionada a través de la propiedad apoderado en la entidad Alumno. El alias e se asigna a esta entidad
+     * Apoderado para su uso en la consulta.
+     *
+     * left join l.domicilio e: esta cláusula left join se utiliza para realizar un join con la entidad Domicilio
+     * relacionada a través de la propiedad domicilio en la entidad Alumno. El alias e se asigna a esta entidad Domicilio
+     * para su uso en la consulta.
+     *
+     * where l.id=:id: esta cláusula where se utiliza para filtrar los resultados de la consulta. En este caso, estamos
+     * filtrando los Alumnos por su ID, utilizando el parámetro :id para representar el valor del ID que se busca.
+     *
+     * public Alumno findByIdAlumno(long id): este es el método de repositorio que define la consulta. Toma un parámetro
+     * long id y devuelve un objeto de tipo Alumno.
+     * @param id
+     * @return
+     */
     @Query(value = "select a from Alumno a left join a.apoderado e left join a.domicilio where a.id=:id")
-    public Alumno findById(long id);
+    public Alumno findByIdAlumno(long id);
 }
